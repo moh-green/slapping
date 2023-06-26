1a98265ff1f749abea971b6aefb3d2abcd6c5df8
Akkibi <88116996+Akkibi@users.noreply.github.com>
2023-06-26 14:46:46 +0200

    file organisation, base short video contact

--- contact.php
+++ contact.php
@@ -1,38 +1,38 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Slapping - Contact</title>
-    <link rel="stylesheet" href="assets/css/contact.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand&family=Sigmar+One&display=swap" rel="stylesheet">
-</head>
+<?php
+
+include 'includes/header.php';
+include 'includes/nav.php';
+include 'includes/footer.php';
+include 'includes/menu.php';
+
+new Head('<link rel="stylesheet" href="assets/css/short.min.css">');
+?>
+
 <body>
-<header>
-    <img src="assets/img/logo.png" alt="logo de Slapping" id="logo">
-       <nav>
-              <ul>
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="video.php">Vidéos</a></li>
-                <li><a href="short.php">Short</a></li>
-                <li><a href="actualite.php">Actualités</a></li>
-                <li><a href="contact.php">À propos</a></li>
-              </ul>
-       </nav> 
-    </header>
-    <main>
-        <h2>Présentation</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione illum, odio ab quos quibusdam tempore similique nisi in, reprehenderit ea nobis laborum eius exercitationem nemo libero perspiciatis rem deserunt suscipit.
-        Sit laboriosam consequuntur ipsum et illum fugiat, dolore commodi! Id ipsam natus facere nemo, doloremque, illum vel explicabo velit nisi quaerat ad voluptate accusamus voluptatibus odit autem necessitatibus maxime nulla.
-        Ex consectetur a eius delectus quis sapiente facere voluptas ad cumque accusantium voluptates, aliquid, totam architecto soluta ipsam asperiores libero enim odio ea quasi aperiam iste placeat nisi distinctio! Possimus.<br>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quos expedita in provident odit cum sed iusto, accusantium inventore. Voluptates necessitatibus laudantium molestiae quia, ea quibusdam nisi iste nihil voluptatibus.
-        Assumenda minima fuga laudantium impedit, itaque nemo explicabo architecto exercitationem blanditiis alias tenetur facere maxime, suscipit qui quo! Sapiente dolore error autem harum commodi qui reprehenderit accusamus ex eum mollitia?
-        </p>
-        <h2>Ils nous font confiance !</h2>
-        <section>
+    <?php
+    new BurgerMenu(false);
+    new NavBar(false, true);
+    ?>
+    <main id="contact-content">
+        <div class="title big-title article-text">
+            <h2>Présentation</h2>
+        </div>
+        <div class="text">
+
+            <b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione illum, odio ab quos quibusdam tempore similique nisi in, reprehenderit ea nobis laborum eius exercitationem nemo libero perspiciatis rem deserunt suscipit.</b>
+            <p>
+                Sit laboriosam consequuntur ipsum et illum fugiat, dolore commodi! Id ipsam natus facere nemo, doloremque, illum vel explicabo velit nisi quaerat ad voluptate accusamus voluptatibus odit autem necessitatibus maxime nulla.
+                Ex consectetur a eius delectus quis sapiente facere voluptas ad cumque accusantium voluptates, aliquid, totam architecto soluta ipsam asperiores libero enim odio ea quasi aperiam iste placeat nisi distinctio! Possimus.</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quos expedita in provident odit cum sed iusto, accusantium inventore. Voluptates necessitatibus laudantium molestiae quia, ea quibusdam nisi iste nihil voluptatibus.</p>
+            , <p>
+                Assumenda minima fuga laudantium impedit, itaque nemo explicabo architecto exercitationem blanditiis alias tenetur facere maxime, suscipit qui quo! Sapiente dolore error autem harum commodi qui reprehenderit accusamus ex eum mollitia?
+            </p>
+        </div>
+        <div class="title big-title article-text">
+            <h2>Ils nous font confiance</h2>
+        </div>
+        <section id="list-logos">
             <img src="assets/img/facebook.png" alt="">
             <img src="assets/img/instagram.png" alt="">
             <img src="assets/img/youtube.png" alt="">
@@ -42,14 +42,9 @@
             <img src="assets/img/facebook.png" alt="">
         </section>
     </main>
-    <footer>
-        <p>Slapping - Le média qui claque</p>
-        <section>
-            <img src="assets/img/facebook.png" alt="">
-            <img src="assets/img/instagram.png" alt="">
-            <img src="assets/img/youtube.png" alt="">
-        </section>
-    </footer>
-    
+    <?php
+    new Footer();
+    ?>
 </body>
+
 </html>
